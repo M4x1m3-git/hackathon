abb095038770007d489c37a1d82de7b2757c6a05
Maxime <>
2025-09-19 11:02:46 +0200

    Phase 2 : Création de toutes les entités + Ajout des fixtures pour avoir des exemples dans la BDD.

--- src/Controller/HackathonController.php
+++ src/Controller/HackathonController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Hackathon;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,18 +19,17 @@ final class HackathonController extends AbstractController
         ]);
     }
 
-    #[Route('/api/hackathons/{id}', name: 'app_hackathon_list')]
-    public function hackathons(EntityManagerInterface $entityManager, $id): Response
+    #[Route('/api/hackathon/{id}', name: 'app_hackathon_list')]
+    public function hackathon(EntityManagerInterface $entityManager, $id): JsonResponse
     {
-        $hackathons = $entityManager->getRepository(Hackathon::class)->find($id);
+        $hackathon = $entityManager->getRepository(Hackathon::class)->find($id);
 
-        if (!$hackathons) {
+        if (!$hackathon) {
             throw $this->createNotFoundException(
                 'No product found for id '.$id
             );
         }
-
-
-        return new Response('Liste des hackathons : '.$hackathons->getLieu());
+        return $this->json(['Lieu' => $hackathon->getLieu()]);
+        // return new Response('Lieu du hackathon : '.$hackathon->getLieu());
     }
 }
